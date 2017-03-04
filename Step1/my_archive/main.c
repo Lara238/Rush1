5936a9b27ca0eb651317105531f37639342d7972
Mystik <mystik@Arch.localdomain>
2017-03-04 13:25:11 +0100

    Add my_archive

---
+++ Step1/my_archive/main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for  in /home/mystik/Projects/Rush1/Step1/my_archive
+** 
+** Made by Mystik
+** Login   <tom.gardelle@epitech.eu>
+** 
+** Started on  Sat Mar  4 11:52:09 2017 Mystik
+** Last update Sat Mar  4 11:52:32 2017 Mystik
+*/
+
+int	main()
+{
+  my_archive();
+  return (0);
+}
