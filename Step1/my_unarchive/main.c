e771c32a4978c55b562ba738c630991cb3768592
Mystik <mystik@Arch.localdomain>
2017-03-04 13:33:19 +0100

    add my_unarchive

---
+++ Step1/my_unarchive/main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for  in /home/mystik/Projects/Rush1/Step1/my_archive
+** 
+** Made by Mystik
+** Login   <tom.gardelle@epitech.eu>
+** 
+** Started on  Sat Mar  4 11:52:09 2017 Mystik
+** Last update Sat Mar  4 13:32:16 2017 Mystik
+*/
+
+int	main()
+{
+  my_unarchive();
+  return (0);
+}
---
+++ Step1/my_unarchive/my_unarchive.c
@@ -0,0 +1,14 @@
+/*
+** archive.c for  in /home/mystik/Projects/Rush1/Step1/my_archive
+** 
+** Made by Mystik
+** Login   <tom.gardelle@epitech.eu>
+** 
+** Started on  Sat Mar  4 11:52:38 2017 Mystik
+** Last update Sat Mar  4 13:32:50 2017 Mystik
+*/
+
+int	my_unarchive()
+{
+  return (0);
+}
